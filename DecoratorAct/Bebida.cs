a606887976bf23c0348bf19257678381970f9171
Lady Nuñez <laadynz@hotmail.com>
2023-10-18 08:23:37 -0700

    Agregar archivos de proyecto.

---
+++ DecoratorAct/Bebida.cs
@@ -0,0 +1,12 @@
+﻿namespace DecoratorAct
+{
+    public class Bebida : IBebida
+    {
+        public string Descripcion => throw new NotImplementedException();
+
+        public double Costo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DecoratorAct/IBebida.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorAct
+{
+    public interface IBebida
+    {
+        string Descripcion { get; }
+        double Costo();
+    }
+}
